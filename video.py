976f47db978861940765fd2edda9c58c673f0437
AnikaJagati <60574348+AnikaJagati@users.noreply.github.com>
2020-02-18 00:09:14 +0530

    Add files via upload

---
+++ video.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret,frame = cap.read()
+    
+    # Our operations on the frame come here
+    im2 = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    #img=cv2.imread('/home/anika/Downloads/green1.jpg',1)
+    #hsv=cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+    
+    lb=np.array([40,100,100])
+    ub=np.array([90,255,255])
+    r=cv2.inRange(im2,lb,ub)
+    im2=cv2.bitwise_and(im2,im2,mask=r)
+    #cv2.imshow('a',r)
+    #cv2.imshow('result',im2)
+    
+#to detect a circle(hough gradient method)
+    im2=cv2.cvtColor(im2,cv2.COLOR_HSV2RGB)
+    im2=cv2.cvtColor(im2,cv2.COLOR_RGB2GRAY)
+    im2=cv2.medianBlur(im2,5)
+    cimg=cv2.cvtColor(im2,cv2.COLOR_GRAY2RGB)
+        
+    circles = cv2.HoughCircles(im2,cv2.HOUGH_GRADIENT,1,150,param1=50,param2=30,minRadius=0,maxRadius=0)
+    circles = np.uint16(np.around(circles))#converting the coordinates to integers
+    #print(circles)
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,0,255),2)#i[0]=x,i[1]=y&i[2]=radius,0,0,255->red color,2=thickness
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(255,0,0),2)#0,0,255->blue for centre
+    
+    cv2.imshow('frame',cimg)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
