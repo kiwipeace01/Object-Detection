c457d30bb4f55bf87115c89022f8689d6d988189
kiwipeace01 <60574300+kiwipeace01@users.noreply.github.com>
2020-02-20 12:32:07 +0530

    Update video.py

--- video.py
+++ video.py
@@ -25,15 +25,21 @@ while(True):
     im2=cv2.cvtColor(im2,cv2.COLOR_RGB2GRAY)
     im2=cv2.medianBlur(im2,5)
     cimg=cv2.cvtColor(im2,cv2.COLOR_GRAY2RGB)
-        
+    
+    '''The function cv2.HoughCircles() contains the following arguments:image, method, dp, minDist,param1,param2,minradius,maxradius.
+    Colored images are converted to grayscale images as we need an 8-bit single channel image,
+    method:method used to detect the circles,minDist:minimum distance between the
+    center coordinates of the circles'''
+    
     circles = cv2.HoughCircles(im2,cv2.HOUGH_GRADIENT,1,150,param1=50,param2=30,minRadius=0,maxRadius=0)
-    circles = np.uint16(np.around(circles))#converting the coordinates to integers
+    circles = np.uint16(np.around(circles))#converting the x-coordinate,y-coordinate of center and radius to integers
+    
     #print(circles)
     for i in circles[0,:]:
         # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,0,255),2)#i[0]=x,i[1]=y&i[2]=radius,0,0,255->red color,2=thickness
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,0,255),2)  #i[0]=x,i[1]=y&i[2]=radius,(0,0,255)->red color,2=thickness
         # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(255,0,0),2)#0,0,255->blue for centre
+        cv2.circle(cimg,(i[0],i[1]),2,(255,0,0),2)  #(255,0,0)->blue for centre
     
     cv2.imshow('frame',cimg)
     if cv2.waitKey(0) & 0xFF == ord('q'):
